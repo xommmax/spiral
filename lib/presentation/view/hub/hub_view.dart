5f0ce8cbf7d2e8de3bac10dde2c57904949616ab
4lfant <4lfant@gmail.com>
2021-07-04 18:11:38 +0300

    Base structure for hub page

---
+++ lib/presentation/view/hub/hub_view.dart
@@ -0,0 +1,13 @@
+import 'package:dairo/presentation/view/base/standard_base_view.dart';
+import 'package:dairo/presentation/view/hub/widgets/widget_hub_view_content.dart';
+import 'package:flutter/material.dart';
+
+import 'hub_viewmodel.dart';
+
+class HubView extends StandardBaseView<HubViewModel> {
+  @override
+  Widget getContent(BuildContext context) => WidgetHubViewContent();
+
+  @override
+  getViewModel() => HubViewModel();
+}
