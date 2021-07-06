23e1c5d257233ac739559f91e804067972f6d177
4lfant <4lfant@gmail.com>
2021-07-06 10:43:38 +0300

    Refactor hub page classes

--- lib/presentation/view/hub/page/hub_page_view.dart
+++ lib/presentation/view/hub/page/hub_page_view.dart
@@ -1,8 +1,8 @@
 import 'package:dairo/presentation/view/base/standard_base_view.dart';
-import 'package:dairo/presentation/view/hub/widgets/widget_hub_view_content.dart';
+import 'package:dairo/presentation/view/hub/page/widgets/widget_hub_page_view_content.dart';
 import 'package:flutter/material.dart';
 
-import 'hub_viewmodel.dart';
+import 'hub_page_viewmodel.dart';
 
 class HubView extends StandardBaseView<HubViewModel> {
   @override
