c2fa9acdf01eb8cd3701d71e653d7a18d98f29c4
dairoinc@gmail.com <123qweasd>
2021-10-13 17:13:30 +0300

    Local chat lib

---
+++ libs/flutter_chat_ui/lib/src/util.dart
@@ -0,0 +1,206 @@
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:intl/intl.dart';
+import './models/date_header.dart';
+import './models/message_spacer.dart';
+import './models/preview_image.dart';
+
+/// Returns text representation of a provided bytes value (e.g. 1kB, 1GB)
+String formatBytes(int size, [int fractionDigits = 2]) {
+  if (size <= 0) return '0 B';
+  final multiple = (log(size) / log(1024)).floor();
+  return (size / pow(1024, multiple)).toStringAsFixed(fractionDigits) +
+      ' ' +
+      ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][multiple];
+}
+
+/// Returns user avatar and name color based on the ID
+Color getUserAvatarNameColor(types.User user, List<Color> colors) =>
+    colors[user.id.hashCode % colors.length];
+
+/// Returns user initials (can have only first letter of firstName/lastName or both)
+String getUserInitials(types.User user) {
+  String initials = '';
+
+  if ((user.firstName ?? '').isNotEmpty) {
+    initials += user.firstName![0].toUpperCase();
+  }
+
+  if ((user.lastName ?? '').isNotEmpty) {
+    initials += user.lastName![0].toUpperCase();
+  }
+
+  return initials.trim();
+}
+
+/// Returns user name as joined firstName and lastName
+String getUserName(types.User user) =>
+    '${user.firstName ?? ''} ${user.lastName ?? ''}'.trim();
+
+/// Returns formatted date used as a divider between different days in the
+/// chat history
+String getVerboseDateTimeRepresentation(
+  DateTime dateTime, {
+  DateFormat? dateFormat,
+  String? dateLocale,
+  DateFormat? timeFormat,
+}) {
+  final formattedDate = dateFormat != null
+      ? dateFormat.format(dateTime)
+      : DateFormat.MMMd(dateLocale).format(dateTime);
+  final formattedTime = timeFormat != null
+      ? timeFormat.format(dateTime)
+      : DateFormat.Hm(dateLocale).format(dateTime);
+  final localDateTime = dateTime.toLocal();
+  final now = DateTime.now();
+
+  if (localDateTime.day == now.day &&
+      localDateTime.month == now.month &&
+      localDateTime.year == now.year) {
+    return formattedTime;
+  }
+
+  return '$formattedDate, $formattedTime';
+}
+
+/// Parses provided messages to chat messages (with headers and spacers) and
+/// returns them with a gallery
+List<Object> calculateChatMessages(
+  List<types.Message> messages,
+  types.User user, {
+  String Function(DateTime)? customDateHeaderText,
+  DateFormat? dateFormat,
+  required int dateHeaderThreshold,
+  String? dateLocale,
+  required int groupMessagesThreshold,
+  required bool showUserNames,
+  DateFormat? timeFormat,
+}) {
+  final chatMessages = <Object>[];
+  final gallery = <PreviewImage>[];
+
+  var shouldShowName = false;
+
+  for (var i = messages.length - 1; i >= 0; i--) {
+    final isFirst = i == messages.length - 1;
+    final isLast = i == 0;
+    final message = messages[i];
+    final messageHasCreatedAt = message.createdAt != null;
+    final nextMessage = isLast ? null : messages[i - 1];
+    final nextMessageHasCreatedAt = nextMessage?.createdAt != null;
+    final nextMessageSameAuthor = message.author.id == nextMessage?.author.id;
+    final notMyMessage = message.author.id != user.id;
+
+    var nextMessageDateThreshold = false;
+    var nextMessageDifferentDay = false;
+    var nextMessageInGroup = false;
+    var showName = false;
+
+    if (showUserNames) {
+      final previousMessage = isFirst ? null : messages[i + 1];
+
+      final isFirstInGroup = notMyMessage &&
+          ((message.author.id != previousMessage?.author.id) ||
+              (messageHasCreatedAt &&
+                  previousMessage?.createdAt != null &&
+                  message.createdAt! - previousMessage!.createdAt! >
+                      groupMessagesThreshold));
+
+      if (isFirstInGroup) {
+        shouldShowName = false;
+        if (message.type == types.MessageType.text) {
+          showName = true;
+        } else {
+          shouldShowName = true;
+        }
+      }
+
+      if (message.type == types.MessageType.text && shouldShowName) {
+        showName = true;
+        shouldShowName = false;
+      }
+    }
+
+    if (messageHasCreatedAt && nextMessageHasCreatedAt) {
+      nextMessageDateThreshold =
+          nextMessage!.createdAt! - message.createdAt! >= dateHeaderThreshold;
+
+      nextMessageDifferentDay =
+          DateTime.fromMillisecondsSinceEpoch(message.createdAt!).day !=
+              DateTime.fromMillisecondsSinceEpoch(nextMessage.createdAt!).day;
+
+      nextMessageInGroup = nextMessageSameAuthor &&
+          nextMessage.createdAt! - message.createdAt! <= groupMessagesThreshold;
+    }
+
+    if (isFirst && messageHasCreatedAt) {
+      chatMessages.insert(
+        0,
+        DateHeader(
+          text: customDateHeaderText != null
+              ? customDateHeaderText(
+                  DateTime.fromMillisecondsSinceEpoch(message.createdAt!),
+                )
+              : getVerboseDateTimeRepresentation(
+                  DateTime.fromMillisecondsSinceEpoch(message.createdAt!),
+                  dateFormat: dateFormat,
+                  dateLocale: dateLocale,
+                  timeFormat: timeFormat,
+                ),
+        ),
+      );
+    }
+
+    chatMessages.insert(0, {
+      'message': message,
+      'nextMessageInGroup': nextMessageInGroup,
+      'showName': notMyMessage &&
+          showUserNames &&
+          showName &&
+          getUserName(message.author).isNotEmpty,
+      'showStatus': true,
+    });
+
+    if (!nextMessageInGroup) {
+      chatMessages.insert(
+        0,
+        MessageSpacer(
+          height: 12,
+          id: message.id,
+        ),
+      );
+    }
+
+    if (nextMessageDifferentDay || nextMessageDateThreshold) {
+      chatMessages.insert(
+        0,
+        DateHeader(
+          text: customDateHeaderText != null
+              ? customDateHeaderText(
+                  DateTime.fromMillisecondsSinceEpoch(nextMessage!.createdAt!),
+                )
+              : getVerboseDateTimeRepresentation(
+                  DateTime.fromMillisecondsSinceEpoch(nextMessage!.createdAt!),
+                  dateFormat: dateFormat,
+                  dateLocale: dateLocale,
+                  timeFormat: timeFormat,
+                ),
+        ),
+      );
+    }
+
+    if (message is types.ImageMessage) {
+      if (kIsWeb) {
+        if (message.uri.startsWith('http')) {
+          gallery.add(PreviewImage(id: message.id, uri: message.uri));
+        }
+      } else {
+        gallery.add(PreviewImage(id: message.id, uri: message.uri));
+      }
+    }
+  }
+
+  return [chatMessages, gallery];
+}
---
+++ libs/flutter_chat_ui/lib/src/widgets/message.dart
@@ -0,0 +1,315 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+
+import '../util.dart';
+import 'file_message.dart';
+import 'image_message.dart';
+import 'inherited_chat_theme.dart';
+import 'inherited_user.dart';
+import 'text_message.dart';
+
+/// Base widget for all message types in the chat. Renders bubbles around
+/// messages and status. Sets maximum width for a message for
+/// a nice look on larger screens.
+class Message extends StatelessWidget {
+  /// Creates a particular message from any message type
+  const Message({
+    Key? key,
+    this.bubbleBuilder,
+    this.customMessageBuilder,
+    this.fileMessageBuilder,
+    this.imageMessageBuilder,
+    required this.message,
+    required this.messageWidth,
+    this.onMessageLongPress,
+    this.onAvatarPressed,
+    this.onMessageTap,
+    this.onPreviewDataFetched,
+    required this.roundBorder,
+    required this.showAvatar,
+    required this.showName,
+    required this.showStatus,
+    required this.showUserAvatars,
+    this.textMessageBuilder,
+    required this.usePreviewData,
+  }) : super(key: key);
+
+  /// Customize the default bubble using this function. `child` is a content
+  /// you should render inside your bubble, `message` is a current message
+  /// (contains `author` inside) and `nextMessageInGroup` allows you to see
+  /// if the message is a part of a group (messages are grouped when written
+  /// in quick succession by the same author)
+  final Widget Function(
+    Widget child, {
+    required types.Message message,
+    required bool nextMessageInGroup,
+  })? bubbleBuilder;
+
+  /// Build a custom message inside predefined bubble
+  final Widget Function(types.CustomMessage, {required int messageWidth})?
+      customMessageBuilder;
+
+  /// Build a file message inside predefined bubble
+  final Widget Function(types.FileMessage, {required int messageWidth})?
+      fileMessageBuilder;
+
+  /// Build an image message inside predefined bubble
+  final Widget Function(types.ImageMessage, {required int messageWidth})?
+      imageMessageBuilder;
+
+  /// Any message type
+  final types.Message message;
+
+  /// Maximum message width
+  final int messageWidth;
+
+  /// Called when user makes a long press on any message
+  final void Function(types.Message)? onMessageLongPress;
+
+  final void Function(types.User)? onAvatarPressed;
+
+  /// Called when user taps on any message
+  final void Function(types.Message)? onMessageTap;
+
+  /// See [TextMessage.onPreviewDataFetched]
+  final void Function(types.TextMessage, types.PreviewData)?
+      onPreviewDataFetched;
+
+  /// Rounds border of the message to visually group messages together.
+  final bool roundBorder;
+
+  /// Show user avatar for the received message. Useful for a group chat.
+  final bool showAvatar;
+
+  /// See [TextMessage.showName]
+  final bool showName;
+
+  /// Show message's status
+  final bool showStatus;
+
+  /// Show user avatars for received messages. Useful for a group chat.
+  final bool showUserAvatars;
+
+  /// Build a text message inside predefined bubble.
+  final Widget Function(
+    types.TextMessage, {
+    required int messageWidth,
+    required bool showName,
+  })? textMessageBuilder;
+
+  /// See [TextMessage.usePreviewData]
+  final bool usePreviewData;
+
+  Widget _avatarBuilder(BuildContext context) {
+    final color = getUserAvatarNameColor(
+      message.author,
+      InheritedChatTheme.of(context).theme.userAvatarNameColors,
+    );
+    final hasImage = message.author.imageUrl != null;
+    final initials = getUserInitials(message.author);
+
+    return showAvatar
+        ? Container(
+            margin: const EdgeInsets.only(right: 8),
+            child: CircleAvatar(
+              backgroundColor: hasImage
+                  ? InheritedChatTheme.of(context)
+                      .theme
+                      .userAvatarImageBackgroundColor
+                  : color,
+              backgroundImage:
+                  hasImage ? NetworkImage(message.author.imageUrl!) : null,
+              radius: 16,
+              child: !hasImage
+                  ? Text(
+                      initials,
+                      style: InheritedChatTheme.of(context)
+                          .theme
+                          .userAvatarTextStyle,
+                    )
+                  : null,
+            ),
+          )
+        : const SizedBox(width: 40);
+  }
+
+  Widget _bubbleBuilder(
+    BuildContext context,
+    BorderRadius borderRadius,
+    bool currentUserIsAuthor,
+  ) {
+    return bubbleBuilder != null
+        ? bubbleBuilder!(
+            _messageBuilder(),
+            message: message,
+            nextMessageInGroup: roundBorder,
+          )
+        : Container(
+            decoration: BoxDecoration(
+              borderRadius: borderRadius,
+              color: !currentUserIsAuthor ||
+                      message.type == types.MessageType.image
+                  ? InheritedChatTheme.of(context).theme.secondaryColor
+                  : InheritedChatTheme.of(context).theme.primaryColor,
+            ),
+            child: ClipRRect(
+              borderRadius: borderRadius,
+              child: _messageBuilder(),
+            ),
+          );
+  }
+
+  Widget _messageBuilder() {
+    switch (message.type) {
+      case types.MessageType.custom:
+        final customMessage = message as types.CustomMessage;
+        return customMessageBuilder != null
+            ? customMessageBuilder!(customMessage, messageWidth: messageWidth)
+            : const SizedBox();
+      case types.MessageType.file:
+        final fileMessage = message as types.FileMessage;
+        return fileMessageBuilder != null
+            ? fileMessageBuilder!(fileMessage, messageWidth: messageWidth)
+            : FileMessage(message: fileMessage);
+      case types.MessageType.image:
+        final imageMessage = message as types.ImageMessage;
+        return imageMessageBuilder != null
+            ? imageMessageBuilder!(imageMessage, messageWidth: messageWidth)
+            : ImageMessage(message: imageMessage, messageWidth: messageWidth);
+      case types.MessageType.text:
+        final textMessage = message as types.TextMessage;
+        return textMessageBuilder != null
+            ? textMessageBuilder!(
+                textMessage,
+                messageWidth: messageWidth,
+                showName: showName,
+              )
+            : TextMessage(
+                message: textMessage,
+                onPreviewDataFetched: onPreviewDataFetched,
+                showName: showName,
+                usePreviewData: usePreviewData,
+              );
+      default:
+        return const SizedBox();
+    }
+  }
+
+  Widget _statusBuilder(BuildContext context) {
+    switch (message.status) {
+      case types.Status.delivered:
+      case types.Status.sent:
+        return InheritedChatTheme.of(context).theme.deliveredIcon != null
+            ? InheritedChatTheme.of(context).theme.deliveredIcon!
+            : Image.asset(
+                'assets/icon-delivered.png',
+                color: InheritedChatTheme.of(context).theme.primaryColor,
+                package: 'flutter_chat_ui',
+              );
+      case types.Status.error:
+        return InheritedChatTheme.of(context).theme.errorIcon != null
+            ? InheritedChatTheme.of(context).theme.errorIcon!
+            : Image.asset(
+                'assets/icon-error.png',
+                color: InheritedChatTheme.of(context).theme.errorColor,
+                package: 'flutter_chat_ui',
+              );
+      case types.Status.seen:
+        return InheritedChatTheme.of(context).theme.seenIcon != null
+            ? InheritedChatTheme.of(context).theme.seenIcon!
+            : Image.asset(
+                'assets/icon-seen.png',
+                color: InheritedChatTheme.of(context).theme.primaryColor,
+                package: 'flutter_chat_ui',
+              );
+      case types.Status.sending:
+        return InheritedChatTheme.of(context).theme.sendingIcon != null
+            ? InheritedChatTheme.of(context).theme.sendingIcon!
+            : Center(
+                child: SizedBox(
+                  height: 10,
+                  width: 10,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.transparent,
+                    strokeWidth: 1.5,
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      InheritedChatTheme.of(context).theme.primaryColor,
+                    ),
+                  ),
+                ),
+              );
+      default:
+        return const SizedBox();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _user = InheritedUser.of(context).user;
+    final _currentUserIsAuthor = _user.id == message.author.id;
+    final _messageBorderRadius =
+        InheritedChatTheme.of(context).theme.messageBorderRadius;
+    final _borderRadius = BorderRadius.only(
+      bottomLeft: Radius.circular(
+        _currentUserIsAuthor || roundBorder ? _messageBorderRadius : 0,
+      ),
+      bottomRight: Radius.circular(_currentUserIsAuthor
+          ? roundBorder
+              ? _messageBorderRadius
+              : 0
+          : _messageBorderRadius),
+      topLeft: Radius.circular(_messageBorderRadius),
+      topRight: Radius.circular(_messageBorderRadius),
+    );
+
+    return Container(
+      alignment:
+          _currentUserIsAuthor ? Alignment.centerRight : Alignment.centerLeft,
+      margin: const EdgeInsets.only(
+        bottom: 4,
+        left: 20,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (!_currentUserIsAuthor && showUserAvatars)
+            InkWell(
+              child: _avatarBuilder(context),
+              onTap: () => onAvatarPressed?.call(message.author),
+            ),
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: messageWidth.toDouble(),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onLongPress: () => onMessageLongPress?.call(message),
+                  onTap: () => onMessageTap?.call(message),
+                  child: _bubbleBuilder(
+                    context,
+                    _borderRadius,
+                    _currentUserIsAuthor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (_currentUserIsAuthor)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Center(
+                child: SizedBox(
+                  height: 16,
+                  width: 16,
+                  child: showStatus ? _statusBuilder(context) : null,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
